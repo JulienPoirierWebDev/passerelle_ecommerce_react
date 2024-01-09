825c0f0ff3e21b9ee55c3adce149279ed5ab444e
JulienPoirierWebDev <connexion@julienpoirier-Webdev.com>
2024-01-09 12:32:47 +0100

    initialisation de features component pour les apprenants

---
+++ src/components/features/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Features() {
+
+  const features = [{
+    icon:"XXX",
+    title:"Next day as standard",
+    content:"Order before 3pm and get your order the next day as standard"
+  },
+  {
+    icon:"",
+    title:"Made by true artisans",
+    content:"Handmade crafted goods made with real passion and craftmanship"
+  },
+  {
+    icon:"",
+    title:"Unbeatable prices",
+    content:"For our materials and quality you won’t find better prices anywhere"
+  },
+  {
+    icon:"",
+    title:"Recycled packaging",
+    content:"We use 100% recycled packaging to ensure our footprint is manageable"
+  }
+]
+
+// utiliser un map pour rendre 4 x le même composant  FeatureBlock
+  
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default Features
